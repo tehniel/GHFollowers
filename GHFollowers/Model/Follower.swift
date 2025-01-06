8f323f26f82c6b5e53f4b34d1410315ffd2477c6
tehniel <skydan.inc@gmail.com>
2025-01-06 15:07:47 -0500

    Planning our Network Calls - API, JSON, Model + BugFix

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,8 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Daniel Ruiz on 1/6/25.
+//
+
+import Foundation
---
+++ GHFollowers/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  GHFollowers
+//
+//  Created by Daniel Ruiz on 1/6/25.
+//
+
+import Foundation
