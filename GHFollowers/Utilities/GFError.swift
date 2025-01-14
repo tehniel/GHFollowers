cb9c15f87eea80a75381d79ac67845de38589fc8
tehniel <skydan.inc@gmail.com>
2025-01-14 10:33:35 -0500

    Result Type Refactor

--- GHFollowers/Utilities/GFError.swift
+++ GHFollowers/Utilities/GFError.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum ErrorMessage: String  {
+enum GFError: String, Error  {
     case invalidUsername = "This username created am invalid request. Please try again."
     case unableToComplete = "Unable to complete your request. Please check your internet connection."
     case invalidResponse = "Invalid response from the sever. Please try again"
