ed935ad011f85b3d58a899d21486a416888e2022
tehniel <skydan.inc@gmail.com>
2025-02-04 14:45:22 -0500

    UICollectionView - Initial Setup

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -10,12 +10,34 @@ import UIKit
 class FollowerListVC: UIViewController {
     
     var username: String!
-
+    var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        congiureCollectionView()
+        configureViewController()
+        getFollowers()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
-        
+    }
+    
+    func congiureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    func getFollowers(){
         NetworkManager.shared.getFollowers(for: username, page: 1) { result in
             
             switch result {
@@ -25,14 +47,10 @@ class FollowerListVC: UIViewController {
                 print(followers)
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happaned", message: error.rawValue, buttonTitle: "Ok")
-
+                
             }
         }
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: true)
+        
     }
     
 }
