fe116016612dfc31655da0ffd9eabf6f361ea4cd
tehniel <skydan.inc@gmail.com>
2025-06-02 11:19:14 -0400

    Loading View

--- GHFollowers/Extensions/UIViewController+Ext.swift
+++ GHFollowers/Extensions/UIViewController+Ext.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+fileprivate var containerView: UIView!
+
 extension UIViewController {
     func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String){
         DispatchQueue.main.async {
@@ -14,6 +16,37 @@ extension UIViewController {
             alertVC.modalPresentationStyle = .overFullScreen
             alertVC.modalTransitionStyle = .crossDissolve
             self.present(alertVC, animated: true)
+        }
+    }
+    
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.25) {
+            containerView.alpha = 0.8
+        }
+        
+        let activityIndicatorView = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicatorView)
+        
+        activityIndicatorView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activityIndicatorView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            activityIndicatorView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor)
+        ])
+        
+        activityIndicatorView.startAnimating()
+    }
+    
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+            containerView.removeFromSuperview()
+            containerView = nil
         }
     }
 }
