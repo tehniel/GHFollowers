d275601f91c09691a19533228b110039886b9e06
tehniel <skydan.inc@gmail.com>
2025-06-02 14:02:53 -0400

    Model Presentation & Passing Data

---
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -0,0 +1,25 @@
+//
+//  UserInfoVC.swift
+//  GHFollowers
+//
+//  Created by Daniel Ruiz on 6/2/25.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+    }
+    
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+}
